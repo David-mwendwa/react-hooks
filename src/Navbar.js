151fa038cc07e7bf7ceb0633ae5fbcfe53a1dce9
David Mwendwa <klatzdavie19@gmail.com>
2021-08-11 08:56:38 +0300

    feat: consume both languageContext and themeContext in the navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -9,21 +9,40 @@ import Switch from '@material-ui/core/Switch';
 import { withStyles } from '@material-ui/core/styles';
 import styles from './styles/NavBarStyles';
 import { ThemeContext } from './contexts/ThemeContext';
+import { withLanguageContext } from './contexts/LanguageContext';
+
+const content = {
+  english: {
+    search: "Search",
+    flag: "🐱"
+  },
+  french: {
+    search: "Chercher",
+    flag: "🐶"
+  },
+  spanish: {
+    search: "Buscar",
+    flag: "🐍"
+  }
+}
 
 class Navbar extends Component {
   static contextType = ThemeContext;
   render() {
     const { isDarkMode, toggleTheme } = this.context;
     const { classes } = this.props;
+    const {language} = this.props.languageContext
+    const {search, flag} = content[language]
+
     return (
       <div className={classes.root}>
         <AppBar position='static' color={isDarkMode ? 'default' : 'primary'}>
           <Toolbar>
             <IconButton className={classes.menuButton} color='inherit'>
-              <span>🎏</span>
+              <span>{flag}</span>
             </IconButton>
             <Typography className={classes.title} variant='h6' color='inherit'>
-              {isDarkMode ? "DARKMODE" : "WHITEMODE"}
+              App Title
             </Typography>
             <Switch onChange={toggleTheme} />
             <div className={classes.grow} />
@@ -32,7 +51,7 @@ class Navbar extends Component {
                 <SearchIcon />
               </div>
               <InputBase
-                placeholder='Search…'
+                placeholder={`${search}...`}
                 classes={{
                   root: classes.inputRoot,
                   input: classes.inputInput,
@@ -46,4 +65,4 @@ class Navbar extends Component {
     );
   }
 }
-export default withStyles(styles)(Navbar);
+export default withLanguageContext(withStyles(styles)(Navbar));
